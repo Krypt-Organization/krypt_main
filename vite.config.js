e3ea6dcda1aeb815d07916ae21d64e74513f4d41
Victor <vector.code11@gmail.com>
2025-01-31 07:50:34 +0000

    fix bundler

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     nodePolyfills({
       exclude: [
-        "fs", // Excludes the polyfill for fs and node:fs
-        ],
+        //"fs", // Excludes the polyfill for fs and node:fs
+      ],
+      overrides: {
+        // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
+        fs: 'memfs',
+      },
     }),
+    react(), 
   ],
   resolve: {
     alias: {
